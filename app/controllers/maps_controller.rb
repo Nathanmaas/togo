8589c00fb1fca420c78256b5d93b5cb15e2f9341
Nathan Maas <nathansmaas@gmail.com>
2015-07-30 01:51:38 -0700

    added yelp api for heroku

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -9,4 +9,15 @@ class MapsController < ApplicationController
     render json: Yelp.client.search('Seattle', parameters)
   end
 
+  private
+  def client
+  Yelp.client.configure do |config|
+  config.consumer_key = ENV['YELP_API']
+  config.consumer_secret = ENV['YELP_SECRET']
+  config.token = ENV['YELP_TOKEN']
+  config.token_secret = ENV['YELP_TOKEN_SECRET']
+  end
+  end
+
 end
+
