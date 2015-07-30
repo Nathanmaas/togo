cd4d5abf7a04e16d3e11ebed6a32b02b10e91532
Nathan Maas <nathansmaas@gmail.com>
2015-07-30 12:30:30 -0700

    added edit and delete todo_item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,8 +6,16 @@ class TodoItemsController < ApplicationController
   end
 
   def create
-    @todo_item = TodoItem.create
-    redirect_to todo_items_path
+      @todo_item = TodoItem.create todo_params
+      redirect_to todo_items_path
+
+    # @todo_item = current_user.todo_item.create todo_params
+    # if todo_item.persisted?
+    #   flash[:success] = "Your link has been posted."
+    #   redirect_to todo_items_path
+    # else
+    #   flash[:danger] = "There was an erroer."
+    #   redirect_to edit_todo_item_path
   end
 
   def new
@@ -20,15 +28,23 @@ class TodoItemsController < ApplicationController
 
   def edit
     @todo_item = TodoItem.find(params[:id])
+    render layout: false
   end
 
   def update
+    @todo_item = TodoItem.find(params[:id])
     @todo_item.update todo_params
-    if todo_item.save
+    if @todo_item.save
     redirect_to todo_items_path
     else
     render :edit
     end
+  end
+
+  def destroy
+    @todo_item = TodoItem.find(params[:id])
+    @todo_item.delete
+    redirect_to todo_items_path
   end
 
   private
